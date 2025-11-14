54235c805f7ed075304bb1505bb09cafc1ccd437
G.Maheshwar Reddy <maheshwar12345reddy@gmail.com>
2025-11-14 11:20:08 +0530

    Update LoginService.java

--- LoginappBackend/src/main/java/com/example/LoginappBackend/LoginService.java
+++ LoginappBackend/src/main/java/com/example/LoginappBackend/LoginService.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Service;
 public interface LoginService {
 	
 	boolean validateUser(String username, String password);
+	int getSalary(String username);
+    
+    boolean userExists(String username);
+    void registerUser(String username, String password, int salary);
 }
